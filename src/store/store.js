d9af6c9fe4b304f000848b11ddff49b568dd1f44
Jayant Verma <iamvermajayant@gmail.com>
2024-12-21 21:55:57 +0530

    reducer fixes

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import fileUploadReducer from "./slices/fileUploadSlice";
+import linkReducer from "./slices/linkSlice";
 
 const store = configureStore({
   reducer: {
